4bd20c181f6a5114b0e05d41663b49e4df86c218
EvgeniyaKa <ved-evgeniya@yandex.ru>
2021-04-02 00:45:35 +0300

    Create

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,8 @@ module.exports = {
                   {
                     loader: 'file-loader',
                     options: {
-                      name: '[name].[ext]'
+                      name: '[name].[ext]',
+                      outputPath: 'img/'
                     }
                   }
                 ]
