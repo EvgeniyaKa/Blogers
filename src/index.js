9d89b5f32df4bd5801c631a52ad5b6bc477474d9
EvgeniyaKa <ved-evgeniya@yandex.ru>
2021-04-02 00:33:14 +0300

    Create

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './public/style/normalize.css';
+import './public/style/scroll.css';
 import './public/style/style.css';
+import './public/style/bloger.css';
+import './public/style/styleMedia.css';
 import './public/script.js';
